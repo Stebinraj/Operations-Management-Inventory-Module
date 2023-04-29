0ba2e6bc6175aad6744248252c35b01fa6893ee7
Stebinraj <stebinraj22@gmail.com>
2023-04-29 20:20:22 +0530

    .

--- backend/routes/inventory/itemsRoutes.js
+++ backend/routes/inventory/itemsRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const itemsModel = require('../../models/Inventory/itemsModel');
+const itemsGroupModel = require('../../models/Inventory/itemsGroupModel');
 const router = express.Router();
 
 // create items
@@ -20,5 +21,34 @@ router.get('/items/:id', async (req, res) => {
     const data = await itemsModel.find({ item_group_id: req.params.id });
     res.send({ success: data });
 });
+
+// aggregate and populate
+
+router.get('/view/items', async (req, res) => {
+    const data = await itemsModel.find({}).populate({
+        path: 'item_group_id',
+        model: itemsGroupModel,
+        lean: true
+    });
+    res.send({ success: data });
+});
+
+
+// router.get('/view/items', async (req, res) => {
+//     const data = await itemsModel.aggregate([
+//         {
+//             $lookup: {
+//                 from: 'item-groups',
+//                 localField: 'item_group_id',
+//                 foreignField: '_id',
+//                 as: 'item_group'
+//             }
+//         }
+//     ]);
+//     res.send({ success: data });
+// });
+
+// aggregate and populate
+
 
 module.exports = router;
