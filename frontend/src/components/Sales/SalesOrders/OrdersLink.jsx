72499a787ccaf0c0d27ca96c066b9ff1903eb1c8
Stebinraj <stebinraj22@gmail.com>
2023-05-07 08:23:32 +0530

    .

--- frontend/src/components/Sales/SalesOrders/OrdersLink.jsx
+++ frontend/src/components/Sales/SalesOrders/OrdersLink.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom'
 const OrdersLink = () => {
     return (
         <>
-            <Link className="nav-link text-dark d-flex p-0" data-bs-toggle="modal" data-bs-target="#customers">
-                <i className="nav-icon bi-cart-check me-1" style={{ fontSize: '21px', color: 'blue' }}></i>
-                <p className='sidebar-links m-auto h6' style={{ color: 'grey' }}>
-                    Orders
-                </p>
+            <Link className="position-relative ms-4 h6 sidebar-links" data-bs-toggle="modal" data-bs-target="#cartItems" style={{ color: 'grey' }}>
+                <i className="bi bi-cart-check me-1" style={{ fontSize: '21px', color: 'blue' }}></i>
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                    2
+                </span>
+                Orders
             </Link>
         </>
     )
