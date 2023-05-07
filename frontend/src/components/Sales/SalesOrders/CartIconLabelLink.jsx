6732ef7c3032c2e788372bdb0d1a883a11b19536
Stebinraj <stebinraj22@gmail.com>
2023-05-07 08:09:37 +0530

    .

--- frontend/src/components/Sales/SalesOrders/CartIconLabelLink.jsx
+++ frontend/src/components/Sales/SalesOrders/CartIconLabelLink.jsx
@@ -5,7 +5,7 @@ const CartIconLabelLink = ({ cartItemsData }) => {
     return (
         <>
             <Link className="position-relative ms-4 h6 sidebar-links" data-bs-toggle="modal" data-bs-target="#cartItems" style={{ color: 'grey' }}>
-                <i className="bi bi-cart me-2" style={{ fontSize: '21px', color: 'blue' }}></i>
+                <i className="bi bi-cart me-1" style={{ fontSize: '21px', color: 'blue' }}></i>
                 <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
                     {cartItemsData.length}
                 </span>
