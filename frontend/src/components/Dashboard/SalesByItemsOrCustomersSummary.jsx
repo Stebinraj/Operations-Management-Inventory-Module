e5771bb41854744844b94789cc41abe278e5342f
Stebinraj <stebinraj22@gmail.com>
2023-05-24 21:04:27 +0530

    ..

--- frontend/src/components/Dashboard/SalesByItemsOrCustomersSummary.jsx
+++ frontend/src/components/Dashboard/SalesByItemsOrCustomersSummary.jsx
@@ -7,23 +7,23 @@ const SalesByItemsOrCustomersSummary = ({ ordersListTable }) => {
                 <div className="card card-primary card-outline">
                     <div className="card-header row">
                         <h5 className="m-0 col-2">Sales</h5>
-                        <div className="d-lg-flex col-10">
-                            <form className='row w-100'>
-                                <div className="mb-0 form-group col-md-6 col-lg-4 d-lg-flex">
+                        <div className="col-10">
+                            <form className='row col-12'>
+                                <div className="mb-0 form-group col-sm-6 col-lg-4 d-lg-flex">
                                     <span className="card-text text-nowrap my-auto me-2">Filter By</span>
                                     <input type="text" className="form-control" placeholder='Enter Item Name' />
                                 </div>
-                                <div className="mb-0 form-group col-md-6 col-lg-5 d-lg-flex">
+                                <div className="mb-0 form-group col-sm-6 col-lg-4 d-lg-flex">
                                     <span className="card-text text-nowrap my-auto me-2">Items / Customer</span>
                                     <input type="text" className="form-control" placeholder='Enter Item Name' />
                                 </div>
-                                <div className="form-group col-lg-2 mb-0">
-                                    <button className='btn btn-primary w-100'>Filter</button>
+                                <div className="form-group col-sm-6 col-lg-2 mb-0">
+                                    <button type='submit' className='btn btn-primary w-100'>Filter</button>
+                                </div>
+                                <div className="form-group col-sm-6 col-lg-2 mt-auto d-lg-flex mb-0">
+                                    <button type='button' className='btn btn-primary w-100' >Reset</button>
                                 </div>
                             </form>
-                            <div className="form-group col-lg-2 mt-auto row d-lg-flex mb-0">
-                                <button className='btn btn-primary w-100' >Reset</button>
-                            </div>
                         </div>
                     </div>
                     <div className="card-body">
