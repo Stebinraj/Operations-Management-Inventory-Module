f5b21b9bc0b6f4746f2ee4d866a7617b39a2931e
Stebinraj <stebinraj22@gmail.com>
2023-05-25 08:23:44 +0530

    .

--- frontend/src/components/Dashboard/SalesByItemsOrCustomersSummary.jsx
+++ frontend/src/components/Dashboard/SalesByItemsOrCustomersSummary.jsx
@@ -9,14 +9,14 @@ const SalesByItemsOrCustomersSummary = ({ ordersListTable }) => {
                         <h5 className="m-0 col-2">Sales</h5>
                         <div className="col-10">
                             <form className='row col-12'>
-                                <div className="mb-2 form-group col-sm-6 col-lg-4 d-lg-flex">
+                                <div className="mb-2 form-group col-sm-6 col-lg-3 d-lg-flex">
                                     <span className="card-text text-nowrap my-auto me-2">Filter By</span>
                                     <select className="form-control">
                                         <option value="" disabled={true} className='text-secondary'>--Select--</option>
                                         <option>hello</option>
                                     </select>
                                 </div>
-                                <div className="mb-2 form-group col-sm-6 col-lg-4 d-lg-flex">
+                                <div className="mb-2 form-group col-sm-6 col-lg-5 d-lg-flex">
                                     <span className="card-text text-nowrap my-auto me-2">Items / Customers</span>
                                     <select className="form-control">
                                         <option value="" disabled={true} className='text-secondary'>--Select--</option>
