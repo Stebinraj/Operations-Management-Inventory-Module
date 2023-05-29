d4560ce244a220da80559fd7447d6d7161bf83b6
Stebinraj <stebinraj22@gmail.com>
2023-05-29 15:41:23 +0530

    ..

--- frontend/src/components/Sales/Customers/AddCustomerModal.jsx
+++ frontend/src/components/Sales/Customers/AddCustomerModal.jsx
@@ -10,31 +10,55 @@ const AddCustomerModal = ({ setName, setEmail, email, setPhoneNumber, phone_numb
                             <h1 className="modal-title fs-5" id="exampleModalLabel">
                                 {updateForm ? 'Update Customer' : 'Create Customer'}
                             </h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleClose}></button>
+                            <button type="button" id='addCustomerModalButton' className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleClose}></button>
                         </div>
                         <div className="modal-body">
                             <form className='row'>
                                 <div className="mb-3 form-group col-12">
                                     <span>Name</span>
-                                    <input type="text" className="form-control" placeholder='Enter Name' onChange={(e) => { setName(e.target.value) }} value={name} />
+                                    <input type="text" className={name.class ? (`form-control ${name.class}`) : ('form-control')} placeholder='Enter Name' onChange={(e) => { setName({ ...name, name: e.target.value }) }} value={name.name} />
+                                    {name.feedback && (
+                                        <>
+                                            <small className="valid-feedback">{name.feedback}</small>
+                                            <small className="invalid-feedback">{name.feedback}</small>
+                                        </>
+                                    )}
                                 </div>
                                 <div className="mb-3 form-group col-12">
                                     <span>Email</span>
-                                    <input type="text" className="form-control" placeholder='Enter Email' onChange={(e) => { setEmail(e.target.value) }} value={email} />
+                                    <input type="text" className={email.class ? (`form-control ${email.class}`) : ('form-control')} placeholder='Enter Email' onChange={(e) => { setEmail({ ...email, email: e.target.value }) }} value={email.email} />
+                                    {email.feedback && (
+                                        <>
+                                            <small className="valid-feedback">{email.feedback}</small>
+                                            <small className="invalid-feedback">{email.feedback}</small>
+                                        </>
+                                    )}
                                 </div>
                                 <div className="mb-3 form-group col-12">
                                     <span>Phone Number</span>
-                                    <input type="text" className="form-control" placeholder='Enter Phone Number' onChange={(e) => { setPhoneNumber(e.target.value) }} value={phone_number} />
+                                    <input type="text" className={phone_number.class ? (`form-control ${phone_number.class}`) : ('form-control')} placeholder='Enter Phone Number' onChange={(e) => { setPhoneNumber({ ...phone_number, phone_number: e.target.value }) }} value={phone_number.phone_number} />
+                                    {phone_number.feedback && (
+                                        <>
+                                            <small className="valid-feedback">{phone_number.feedback}</small>
+                                            <small className="invalid-feedback">{phone_number.feedback}</small>
+                                        </>
+                                    )}
                                 </div>
                                 <div className="mb-3 form-group col-12">
                                     <span>Billing Address</span>
-                                    <input type="text" className="form-control" placeholder='Enter Billing Address' onChange={(e) => { setBillingAddress(e.target.value) }} value={billing_address} />
+                                    <input type="text" className={billing_address.class ? (`form-control ${billing_address.class}`) : ('form-control')} placeholder='Enter Billing Address' onChange={(e) => { setBillingAddress({ ...billing_address, billing_address: e.target.value }) }} value={billing_address.billing_address} />
+                                    {billing_address.feedback && (
+                                        <>
+                                            <small className="valid-feedback">{billing_address.feedback}</small>
+                                            <small className="invalid-feedback">{billing_address.feedback}</small>
+                                        </>
+                                    )}
                                 </div>
                             </form>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={handleClose}>Close</button>
-                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={(e) => { updateForm ? (updateCustomer(e)) : (addCustomer(e)) }}>Submit</button>
+                            <button type="button" className="btn btn-primary" onClick={(e) => { updateForm ? (updateCustomer(e)) : (addCustomer(e)) }}>Submit</button>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/Sales/Customers/Customers.jsx
+++ frontend/src/components/Sales/Customers/Customers.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
+import * as EmailValidator from 'email-validator';
 import AddCustomerButton from './AddCustomerButton';
 import AddCustomerModal from './AddCustomerModal';
 import CustomerLists from './CustomerLists';
@@ -8,24 +9,24 @@ import CustomerListTable from './CustomerListTable';
 
 const Customers = () => {
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone_number, setPhoneNumber] = useState('');
-    const [billing_address, setBillingAddress] = useState('');
+    const [name, setName] = useState({ name: '', class: '', feedback: '' });
+    const [email, setEmail] = useState({ email: '', class: '', feedback: '' });
+    const [phone_number, setPhoneNumber] = useState({ phone_number: '', class: '', feedback: '' });
+    const [billing_address, setBillingAddress] = useState({ billing_address: '', class: '', feedback: '' });
     // conditionally render form create and update user
     const [updateForm, setupdateForm] = useState(false);
+    const nameRegex = /^[A-Za-z\s]+$/;
 
     // state variable of customers datas
     const [customerData, setCustomerData] = useState([]);
     const [id, setId] = useState('');
 
     // create / update form close to set variables empty 
-    const handleClose = async (e) => {
-        e.preventDefault();
-        setName('');
-        setEmail('');
-        setPhoneNumber('');
-        setBillingAddress('');
+    const handleClose = async () => {
+        setName({ name: '', class: '', feedback: '' });
+        setEmail({ email: '', class: '', feedback: '' });
+        setPhoneNumber({ phone_number: '', class: '', feedback: '' });
+        setBillingAddress({ billing_address: '', class: '', feedback: '' });
         setupdateForm(false);
     };
 
@@ -33,14 +34,24 @@ const Customers = () => {
     const addCustomer = async (e) => {
         try {
             e.preventDefault();
-            const response = await axios.post('http://localhost:5000/customer', { name, email, phone_number, billing_address });
-            if (response && response.data.success) {
-                toast.success('Customer Added Successfully !!!');
-                setName('');
-                setEmail('');
-                setPhoneNumber('');
-                setBillingAddress('');
-                await getCustomer();
+            if (await ValidateName() & await validateEmail() & await validatePhoneNumber() & await validateBillingAddress()) {
+                const response = await axios.post('http://localhost:5000/customer', {
+                    name: name.name,
+                    email: email.email,
+                    phone_number: phone_number.phone_number,
+                    billing_address: billing_address.billing_address
+                });
+                if (response && response.data.success) {
+                    toast.success('Customer Added Successfully !!!');
+                    setTimeout(() => {
+                        setName({ name: '', class: '', feedback: '' });
+                        setEmail({ email: '', class: '', feedback: '' });
+                        setPhoneNumber({ phone_number: '', class: '', feedback: '' });
+                        setBillingAddress({ billing_address: '', class: '', feedback: '' });
+                        document.getElementById("addCustomerModalButton").click();
+                    }, 1000);
+                    await getCustomer();
+                }
             }
         } catch (error) {
             console.log(error.message);
@@ -53,7 +64,6 @@ const Customers = () => {
             const response = await axios.get('http://localhost:5000/customer');
             if (response && response.data.success) {
                 setCustomerData(response.data.success);
-                setupdateForm(false);
             }
         } catch (error) {
             console.error(error.message);
@@ -64,10 +74,10 @@ const Customers = () => {
     const handleUpdateData = async (e, value) => {
         e.preventDefault();
         setId(value._id);
-        setName(value.name);
-        setEmail(value.email);
-        setPhoneNumber(value.phone_number);
-        setBillingAddress(value.billing_address);
+        setName({ ...name, name: value.name });
+        setEmail({ ...email, email: value.email });
+        setPhoneNumber({ ...phone_number, phone_number: value.phone_number });
+        setBillingAddress({ ...billing_address, billing_address: value.billing_address });
         setupdateForm(true);
     };
 
@@ -75,18 +85,93 @@ const Customers = () => {
     const updateCustomer = async (e) => {
         try {
             e.preventDefault();
-            const response = await axios.put(`http://localhost:5000/customer/${id}`, { name, email, phone_number, billing_address });
-            if (response && response.data.success) {
-                setName('');
-                setEmail('');
-                setPhoneNumber('');
-                setBillingAddress('');
-                setId('');
-                toast.success('Customer Updated !!!');
-                await getCustomer();
-            }
+            if (await ValidateName() & await validateEmail() & await validatePhoneNumber() & await validateBillingAddress()) {
+                const response = await axios.put(`http://localhost:5000/customer/${id}`, {
+                    name: name.name,
+                    email: email.email,
+                    phone_number: phone_number.phone_number,
+                    billing_address: billing_address.billing_address
+                });
+                if (response && response.data.success) {
+                    setId('');
+                    setTimeout(() => {
+                        setName({ name: '', class: '', feedback: '' });
+                        setEmail({ email: '', class: '', feedback: '' });
+                        setPhoneNumber({ phone_number: '', class: '', feedback: '' });
+                        setBillingAddress({ billing_address: '', class: '', feedback: '' });
+                        document.getElementById("addCustomerModalButton").click();
+                        setupdateForm(false);
+                    }, 1000);
+                    toast.success('Customer Updated !!!');
+                    await getCustomer();
+                }
+            };
         } catch (error) {
             console.log(error.message);
+        }
+    };
+
+    // validate name
+    const ValidateName = async () => {
+        if (name.name === "") {
+            setName({ ...name, feedback: 'Required *', class: 'is-invalid' });
+            return false;
+        } else if (nameRegex.test(name.name)) {
+            setName({ ...name, feedback: 'All Good', class: 'is-valid' });
+            return true;
+        } else {
+            setName({ ...name, feedback: 'Text Only Accepted', class: 'is-invalid' });
+            return false;
+        }
+    };
+
+    // validate email
+    const validateEmail = async () => {
+        if (email.email === "") {
+            setEmail({ ...email, feedback: 'Required *', class: 'is-invalid' });
+            return false;
+        }
+        else if (EmailValidator.validate(email.email)) {
+            setEmail({ ...email, feedback: 'All Good', class: 'is-valid' });
+            return true;
+        }
+        else {
+            setEmail({ ...email, feedback: 'Invalid Email', class: 'is-invalid' });
+            return false;
+        }
+    };
+
+    // validate phone number
+    const validatePhoneNumber = async () => {
+        const pattern_1 = /^\d{10}$/;
+        const pattern_2 = /^[1-9]\d{2}[-][1-9]\d{2}[-]\d{4}$/;
+        const pattern_3 = /^[1-9]\d{2}\.\d{3}\.\d{4}$/;
+        const pattern_4 = /^[1-9]\d{2}\s\d{3}\s\d{4}$/;
+
+        if (pattern_1.test(phone_number.phone_number) || pattern_2.test(phone_number.phone_number) || pattern_3.test(phone_number.phone_number) || pattern_4.test(phone_number.phone_number)) {
+            setPhoneNumber({ ...phone_number, feedback: 'All Good', class: 'is-valid' });
+            return true;
+        } else if (phone_number.phone_number == "") {
+            setPhoneNumber({ ...phone_number, feedback: 'Required *', class: 'is-invalid' });
+            return false;
+        } else {
+            setPhoneNumber({ ...phone_number, feedback: 'Invalid Phone Number', class: 'is-invalid' });
+            return false;
+        }
+    };
+
+    // Validate billing Address
+    const validateBillingAddress = async () => {
+        const address_pattern = /^([a-zA-Z0-9\s,-]+)$/;
+        if (address_pattern.test(billing_address.billing_address)) {
+            setBillingAddress({ ...billing_address, feedback: 'All Good', class: 'is-valid' });
+            return true;
+        } else if (billing_address.billing_address === "") {
+            setBillingAddress({ ...billing_address, feedback: 'Required *', class: 'is-invalid' });
+            return false;
+        } else {
+            setBillingAddress({ ...billing_address, feedback: 'Invalid Address', class: 'is-invalid' });
+            return false;
         }
     }
 
